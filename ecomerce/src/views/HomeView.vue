fb2cda5174b24b6f06fd73503066dbeb04290334
takiguchii <alexandre.pupim360@gmail.com>
2025-05-29 21:23:33 -0300

    Css: Stylizing the letters of the site logo

--- ecomerce/src/views/HomeView.vue
+++ ecomerce/src/views/HomeView.vue
@@ -6,8 +6,9 @@
       style="background-image: url('https://img.freepik.com/fotos-gratis/luminaria-de-neon-para-vendas-de-sexta-feira-negra_23-2151833076.jpg?semt=ais_hybrid&w=740');"
     >
       <!-- Frase em cima da imaghem de fundo  -->
-      <div class="h-full flex items-center justify-center text-white text-3xl font-bold bg-black/60">
-        Imagem do Fundo do site 
+      <div class="h-full flex flex-col justify-center pl-20 text-white bg-black/60">
+      <h1 class="text-6xl font-bold mb-4 tracking-wide">TechMart</h1>
+      <p class="text-2xl font-light italic">Aqui você encontra aquilo que busca.</p> 
       </div>
     </div>
 
