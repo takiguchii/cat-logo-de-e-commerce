4fa5247ecf42326a011e214fb5aeb65a13396c4d
takiguchii <alexandre.pupim360@gmail.com>
2025-05-29 21:15:37 -0300

    fix: Correcting spelling mistakes on the home screen

--- ecomerce/src/views/HomeView.vue
+++ ecomerce/src/views/HomeView.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="min-h-screen flex flex-col bg-gray-100">
-    <!-- Parte superior com imagem de fundo -->
+    <!-- espaçamento  -->
     <div
       class="w-full h-[400px] bg-cover bg-center bg-no-repeat"
       style="background-image: url('https://via.placeholder.com/1200x400');"
     >
-      <!-- Conteúdo sobre a imagem, se quiser -->
+      <!-- Frase em cima da imaghem de fundo  -->
       <div class="h-full flex items-center justify-center text-white text-3xl font-bold bg-black/60">
-        Imagem de fundo do site
+        Imagem do Fundo do site 
       </div>
     </div>
 
-    <!-- Conteúdo abaixo -->
+    <!-- Conteudo do ecomerce  -->
     <main class="flex-1 flex justify-center items-center">
       <div class="w-full max-w-3xl bg-white rounded-3xl p-10 shadow">
-        Conteúdo da página
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, molestias? Aut possimus, ut soluta itaque consequatur ea nisi eligendi temporibus veritatis quidem vero inventore suscipit ipsam provident dolorem rem ad.
       </div>
     </main>
   </div>
