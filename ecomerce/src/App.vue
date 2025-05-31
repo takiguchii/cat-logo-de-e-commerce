d4b27e40b551c693eaf3a975ff69b59b22efc2d2
Racx-DEV <raulfabiano.dev@gmail.com>
2025-05-30 21:15:07 -0300

    feat: sidebar created for easier navigation

--- ecomerce/src/App.vue
+++ ecomerce/src/App.vue
@@ -1,16 +1,30 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterView } from 'vue-router'
 import Navbar from './components/NavbarComponent.vue'
 import Footer from './components/FooterComponent.vue'
+import Sidebarcomponent from './components/Sidebarcomponent.vue'
+
+const sidebarAberta = ref(false)
+
+function alternarSidebar() {
+  sidebarAberta.value = !sidebarAberta.value
+}
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col">
-    <Navbar />
+  <div class="min-h-screen flex flex-col relative">
+    <!-- Sidebar -->
+    <Sidebarcomponent v-if="sidebarAberta" />
+
+    <!-- Navbar com botão que emite toggle -->
+    <Navbar @toggle-sidebar="alternarSidebar" />
+
+    <!-- Conteúdo principal -->
     <main class="flex-grow">
       <RouterView />
     </main>
+
     <Footer />
   </div>
 </template>
-
---
+++ ecomerce/src/components/Sidebarcomponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="fixed top-0 left-0 w-64 h-full rounded-r-2xl bg-neutral-900 text-white z-50 shadow-lg">
+    <div class="p-6 text-xl font-bold border-b border-neutral-800">
+      TechMart
+    </div>
+    <nav class="p-4 space-y-3">
+      <RouterLink to="/" class="block py-2 px-4 rounded hover:bg-neutral-700">início</RouterLink>
+      <RouterLink to="/produtos" class="block py-2 px-4 rounded hover:bg-neutral-700">Produtos</RouterLink>
+      <RouterLink to="/categorias" class="block py-2 px-4 rounded hover:bg-neutral-700">Categorias ></RouterLink>
+      <RouterLink to="/contato" class="block py-2 px-4 rounded hover:bg-neutral-700">Contato</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
