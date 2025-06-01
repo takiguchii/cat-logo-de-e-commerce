2d9752b5dbca634ef93b84abb230f4f449dca98e
takiguchi <alexandre.pupim360@gmail.com>
2025-06-01 10:02:13 -0300

    Css: Style in footer

--- ecomerce/src/components/FooterComponent.vue
+++ ecomerce/src/components/FooterComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-gray-800 text-white py-6 mt-auto">
+  <footer class="bg-[#080C1C] text-white py-6 mt-auto">
     <div class="container mx-auto text-center">
       <h2 class="text-xl font-bold mb-2">TechMart</h2>
       <p class="text-xs text-gray-400">Aqui você encontra o que procura</p>
